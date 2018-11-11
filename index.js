7ddae6a75e1014ad5bad0dd95ed3a49974deb9ba
ShankarSSS <35001879+ShankarSSS@users.noreply.github.com>
2018-11-11 18:04:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,21 +15,25 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
-  var speech =
-    req.body.queryResult &&
-    req.body.queryResult.parameters &&
-    req.body.result.parameters.echoText
-      ? req.body.result.parameters.echoText
-      : "Seems like some problem. Speak again.";
- 
-  // Get the date for the weather forecast (if present)
-  let date = '';
-  if (req.body.queryResult.parameters['date']) {
-    date = req.body.queryResult.parameters['date'];
-    console.log('Date: ' + date);
-  }
   
-  return res.json({ 'fulfillmentText': 'Shankarnath' });
+  const agent = new WebhookClient({ request, response });
+
+  function makeAppointment (agent) {
+    // Use the Dialogflow's date and time parameters to create Javascript Date instances, 'dateTimeStart' and 'dateTimeEnd',
+    // which are used to specify the appointment's time.
+    const appointmentDuration = 1;// Define the length of the appointment to be one hour.
+    const dateTimeStart = convertParametersDate(agent.parameters.date, agent.parameters.time);
+    const dateTimeEnd = addHours(dateTimeStart, appointmentDuration);
+    const appointmentTimeString = getLocaleTimeString(dateTimeStart);
+    const appointmentDateString = getLocaleDateString(dateTimeStart);
+   return res.json({ 'fulfillmentText': 'Success' });
+  }
+  let intentMap = new Map();
+  intentMap.set('Make Appointment', makeAppointment);  // It maps the intent 'Make Appointment' to the function 'makeAppointment()'
+  agent.handleRequest(intentMap);
+});
+  
+ 
   
 });
 
