a06613fd098a3d6cb63f1098f753874c2551b7a2
ShankarSSS <35001879+ShankarSSS@users.noreply.github.com>
2018-11-11 16:44:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,11 +20,9 @@ restService.post("/echo", function(req, res) {
     req.body.result.parameters.echoText
       ? req.body.result.parameters.echoText
       : "Seems like some problem. Speak again.";
-  return res.json({
-    speech: speech,
-    displayText: speech,
-    source: "webhook-echo-sample"
-  });
+ 
+  return res.json({ 'fulfillmentText': 'Shankarnath' });
+  
 });
 
 restService.post("/audio", function(req, res) {
