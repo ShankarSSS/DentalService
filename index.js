ecc1f62cd45f739cfe4f37c7fcebd961f61a3430
ShankarSSS <35001879+ShankarSSS@users.noreply.github.com>
2018-11-11 18:36:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const functions = require('firebase-functions');
+const {google} = require('googleapis');
+const {WebhookClient} = require('dialogflow-fulfillment');
+
 
 const restService = express();
 
@@ -15,11 +19,44 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
-  
-  
- return res.json({ 'fulfillmentText': 'Success string' });
-  
+ const agent = new WebhookClient({ request, response });
+
+  function makeAppointment (agent) {
+    // Use the Dialogflow's date and time parameters to create Javascript Date instances, 'dateTimeStart' and 'dateTimeEnd',
+    // which are used to specify the appointment's time.
+    const appointmentDuration = 1;// Define the length of the appointment to be one hour.
+    const dateTimeStart = convertParametersDate(agent.parameters.date, agent.parameters.time);
+    const dateTimeEnd = addHours(dateTimeStart, appointmentDuration);
+    const appointmentTimeString = getLocaleTimeString(dateTimeStart);
+    const appointmentDateString = getLocaleDateString(dateTimeStart);
+   return res.json({ 'fulfillmentText': 'Success' });
+  }
+  let intentMap = new Map();
+  intentMap.set('Make Appointment', makeAppointment);  // It maps the intent 'Make Appointment' to the function 'makeAppointment()'
+  agent.handleRequest(intentMap);
 });
+
+});
+
+// A helper function that receives Dialogflow's 'date' and 'time' parameters and creates a Date instance.
+function convertParametersDate(date, time){
+  return new Date(Date.parse(date.split('T')[0] + 'T' + time.split('T')[1].split('-')[0] + timeZoneOffset));
+}
+
+// A helper function that adds the integer value of 'hoursToAdd' to the Date instance 'dateObj' and returns a new Data instance.
+function addHours(dateObj, hoursToAdd){
+  return new Date(new Date(dateObj).setHours(dateObj.getHours() + hoursToAdd));
+}
+
+// A helper function that converts the Date instance 'dateObj' into a string that represents this time in English.
+function getLocaleTimeString(dateObj){
+  return dateObj.toLocaleTimeString('en-US', { hour: 'numeric', hour12: true, timeZone: timeZone });
+}
+
+// A helper function that converts the Date instance 'dateObj' into a string that represents this date in English. 
+function getLocaleDateString(dateObj){
+  return dateObj.toLocaleDateString('en-US', { weekday: 'long', month: 'long', day: 'numeric', timeZone: timeZone });
+}
 
 restService.post("/audio", function(req, res) {
   var speech = "";
