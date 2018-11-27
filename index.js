a3a88ba9b1b21c63ca19841c9778f96c2b9b66a6
Shankarnath <shankarnath.mailme@gmail.com>
2018-11-27 19:53:04 +0530

    Push from local repo

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 "use strict";
-
+//Source for api call
+//https://www.thepolyglotdeveloper.com/2017/10/consume-remote-api-data-nodejs-application/
 const express = require("express");
 const bodyParser = require("body-parser");
+var Request = require("request");
 
-const functions = require('firebase-functions');
+
 const {google} = require('googleapis');
 const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
 
 
 const restService = express();
@@ -18,42 +21,147 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-restService.post("/echo", function(request, response) {
+const timeZone = 'Asia/Calcutta';  // Change it to your time zone
+const timeZoneOffset = '+05:30';  
+
+//var result='';
+
+restService.post("/", function(request, response) {
  
    const agent = new WebhookClient({ request, response });
   
 
   function makeAppointment (agent) {
-    // Use the Dialogflow's date and time parameters to create Javascript Date instances, 'dateTimeStart' and 'dateTimeEnd',
+   
+  }
+
+  function productList (agent) {    
+    //agent.add(productList); 
+   // agent.add(`Shankarnath : `+result); 
+  }
+
+  function appointmentList (agent) {    
+  
+  }
+
+  function GetProducts(intentMap){
+
+    Request.get("https://dental-services-api.herokuapp.com/products/List", (error, response, body) => {
+      if(error) {
+          return console.dir(error);
+      }
+     // console.dir(JSON.parse(body));
+     var objectsOfObjects = JSON.parse(body);
+
+     var productList='We are providing following services, ';   
+      //iterating through 'objects of objects'
+      for (var keyVal in objectsOfObjects) {
+        if (objectsOfObjects.hasOwnProperty(keyVal)) {
+         // Since dynamic, use [] since the key isn't literally named "keyVal"
+          var obj = objectsOfObjects[keyVal]; 
+          // NOW you can treat it like an obj
+          productList += obj.name +' ,';
+          //agent.add(new Suggestion(obj.name));
+        }
+    }
+    //result=productList;
+    productList = productList.slice(0,productList.length-1)
+    agent.add(productList);
+
+    // agent.add(new Card({
+    //         title: `Title: this is a card title`,
+    //         imageUrl: 'https://dialogflow.com/images/api_home_laptop.svg',
+    //         text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
+    //         buttonText: 'This is a button',
+    //         buttonUrl: 'https://docs.dialogflow.com/'
+    //       })
+    //     );
+    //     agent.add(new Suggestion(`Quick Reply`));
+    //     agent.add(new Suggestion(`Suggestion`));
+    //     agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
+
+    agent.handleRequest(intentMap);    
+  });  
+  }
+
+  function GetAppointments(intentMap){
+
+    Request.get("https://dental-services-api.herokuapp.com/appointments/List", (error, response, body) => {
+      if(error) {
+          return console.dir(error);
+      }
+     // console.dir(JSON.parse(body));
+     var objectsOfObjects = JSON.parse(body);
+
+     var appointmentList='';   
+      //iterating through 'objects of objects'
+      for (var keyVal in objectsOfObjects) {
+        if (objectsOfObjects.hasOwnProperty(keyVal)) {
+         // Since dynamic, use [] since the key isn't literally named "keyVal"
+          var obj = objectsOfObjects[keyVal]; 
+          // NOW you can treat it like an obj
+          appointmentList += 'Patient Name : '+obj.name+', Appointment Date & Time : '+obj.appointment_start_date +'<br>';
+          //agent.add(new Suggestion(obj.name));
+        }
+    }
+    //result=productList;
+    //productList = productList.slice(0,productList.length-1)
+    agent.add(appointmentList);
+
+    agent.handleRequest(intentMap);    
+  });  
+  }
+  
+function SetAppointment(intentMap){
+ // Use the Dialogflow's date and time parameters to create Javascript Date instances, 'dateTimeStart' and 'dateTimeEnd',
     // which are used to specify the appointment's time.
     const appointmentDuration = 1;// Define the length of the appointment to be one hour.
     const dateTimeStart = convertParametersDate(agent.parameters.date, agent.parameters.time);
     const dateTimeEnd = addHours(dateTimeStart, appointmentDuration);
     const appointmentTimeString = getLocaleTimeString(dateTimeStart);
     const appointmentDateString = getLocaleDateString(dateTimeStart);
-    
-    $.ajax( { url: "https://api.mlab.com/api/1/dat",
-		  data: JSON.stringify(
-        { 
-          "Name" : '',
-          "AppointmentDate" : agent.parameters.date,
-          "AppointmentStart": dateTimeStart,
-          "AppointmentEnd": dateTimeEnd
-        } ),
-		  type: "POST",
-      contentType: "application/json",
-      success:function(data){
-           agent.add(`Got it. I have your appointment scheduled on ${appointmentDateString} at ${appointmentTimeString}. See you soon. Good-bye.`);
-      },
-      error:function(xhr,status,err){
-       agent.add(`Sorry, we're booked not able to book. Is there anything else I can do for you?`);
+
+    var currentDateTime = new Date().toLocaleString('en-US', {
+      timeZone: timeZone
+    });
+
+    Request.post({
+      "headers": { "content-type": "application/json" },
+      "url": "https://dental-services-api.herokuapp.com/appointments/",
+      "body": JSON.stringify({         
+          "name":agent.parameters.name,
+          "mobile":agent.parameters.phoneNumber,
+          "email":"shankar@gmail.com",
+          "source":"",
+          "appointment_date":agent.parameters.date,
+          "appointment_start_date":dateTimeStart,
+          "appointment_end_date":dateTimeEnd,
+          "created_date":currentDateTime,
+          "created_by":""
+      })
+  }, (error, response, body) => {
+      if(error) {
+          return console.dir(error);
       }
-    } );
-    
-  }
+      //console.dir(JSON.parse(body));
+      agent.add('Dear '+agent.parameters.name+ ', your appointment scheduled on '+ appointmentDateString +' at '+ appointmentDateString +'. See you soon. Good-bye');
+      agent.handleRequest(intentMap);  
+  });    
+}
+
   let intentMap = new Map();
-  intentMap.set('Make Appointment', makeAppointment);  // It maps the intent 'Make Appointment' to the function 'makeAppointment()'
-  agent.handleRequest(intentMap);
+  if (agent.intent == 'Make Appointment') {
+    intentMap.set('Make Appointment', makeAppointment);  // It maps the intent 'Make Appointment' to the function 'makeAppointment()'
+    SetAppointment(intentMap);
+  }
+  else if (agent.intent == 'Product List') {
+    intentMap.set('Product List', productList); 
+    GetProducts(intentMap);
+  }
+  else if (agent.intent == 'Appointment List') {
+    intentMap.set('Appointment List', appointmentList); 
+    GetAppointments(intentMap);
+  }
 
 });
 
@@ -62,7 +170,7 @@ restService.post("/echo", function(request, response) {
 
 // A helper function that receives Dialogflow's 'date' and 'time' parameters and creates a Date instance.
 function convertParametersDate(date, time){
-  return new Date(Date.parse(date.split('T')[0] + 'T' + time.split('T')[1].split('-')[0] + timeZoneOffset));
+  return new Date(Date.parse(date.split('T')[0] + 'T' + time.split('T')[1].split('+')[0] + timeZoneOffset));
 }
 
 // A helper function that adds the integer value of 'hoursToAdd' to the Date instance 'dateObj' and returns a new Data instance.
@@ -81,6 +189,6 @@ function getLocaleDateString(dateObj){
 }
 
 
-restService.listen(process.env.PORT || 8000, function() {
-  console.log("Server up and listening");
+restService.listen(8000, function() {
+  console.log("Server up and listening 8000");
 });
